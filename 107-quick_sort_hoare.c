b5ed6dcdfc58639cb36fe7df4eb8a662f2efbb78
Mnqobi Donga <kevedonga1017@gmail.com>
2023-09-26 10:29:30 -0700

    Another function added

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+/**
+ * swap - swap variables
+ * @a: first letter
+ * @b: second letter
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * hoare_partition - sorts algorithm quick
+ * @array: array to sort from
+ * @low:  low array
+ * @high: top array
+ * Return: j
+ */
+int hoare_partition(int *array, int low, int high)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j = high + 1;
+	int k;
+
+	while (1)
+	{
+		do {
+			i++;
+		} while (array[i] < pivot);
+		do {
+			j--;
+		} while (array[j] > pivot);
+		if (i >= j)
+		{
+			return (j);
+		}
+	}
+	swap(&array[i], &array[j]);
+	printf("Swap: ");
+	for (k = low; k <= high; k++)
+	{
+		printf("%d ", array[k]);
+	}
+	printf("\n");
+}
+
+/**
+ * quick_sort_hoare - function that sorts the algorithm quick
+ * @array: array to sort from
+ * @high: high array
+ * @low: low array
+ */
+void quick_sort_hoare(int *array, int low, int high)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = hoare_partition(array, low, high);
+		quick_sort_hoare(array, low, pivot_index);
+		quick_sort_hoare(array, pivot_index + 1, high);
+	}
+}
+
+/**
+ * print_array - array to print
+ * @array: array to sort from
+ * @size: size of the array
+ */
+void print_array(int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+}
+
