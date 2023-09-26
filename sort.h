d9ea45ec6542a39852e3c8f6e8fcdf10c8c38763
Mnqobi Donga <kevedonga1017@gmail.com>
2023-09-26 10:41:21 -0700

    Added in sort.h

--- sort.h
+++ sort.h
@@ -24,7 +24,10 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void swap(int *a, int *b);
+int hoare_partition(int *array, int low, int high);
+void quick_sort_hoare(int *array, int low, int high);
+void print_array(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 
 #endif
